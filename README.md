ea0f2c9805f2268e98ed88acc7079cb0f57389ac
Oliver Crowe <olivercampbellcrowe@outlook.com>
2019-09-08 13:19:06 +0100

    Initial Files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HelperProject
 A website that allows you to post guides and questions relating to Programming, Coding, Hacking and Hardware.
+Written in PHP, using MySQL
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <!-- Base Meta Tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--- Mobile responsive -->
+
+        <!-- Custom Meta Tags -->
+        <title>HelperProj - Yet to be named</title>
+        <meta name="description" content="Description yet to be filled in">
+        <meta name="keywords" content="keyword1,keyword,2">
+        <meta name="author" content="Oliver Crowe">
+
+        <!-- Links, custom and more - Fonts, Icons, Stylesheets -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/111905a993.js"></script>
+        <link rel="stylesheet" href="style/style.css">
+
+    </head>
+    <body>
+
+    </body>
+</html>
