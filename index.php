e44beb9fea9e94713b48e517053bfaf2edf32076
Josh Powell <olivercampbellcrowe@outlook.com>
2019-09-08 17:47:33 +0100

    JS Added

--- index.php
+++ index.php
@@ -20,5 +20,12 @@
     </head>
     <body>
 
+
+    <!-- Javascript -->
+    <script
+	src="https://code.jquery.com/jquery-3.4.1.min.js"
+	integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+	crossorigin="anonymous"></script>
+    <script src="js/index.js"></script>
     </body>
 </html>
